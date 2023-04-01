8fe4eece0490df897afcd0d60148aaebdee01c8a
Alina Rublea <rublea@adobe.com>
2023-04-02 01:07:16 +0300

    simplify collapsible block

--- blocks/collapse/collapse.js
+++ blocks/collapse/collapse.js
@@ -3,11 +3,13 @@ function addCollapseListener(collapseBlock) {
   const collapsibleSection = collapseBlock.parentElement.parentElement;
   const expandButton = collapseBlock.querySelector('div:nth-child(3)');
   const collapseButton = collapseBlock.querySelector('div:nth-child(4)');
-  const buttonsParent = expandButton.parentElement;
-  const arrowElement = document.createElement('div');
-  arrowElement.classList.add('arrow');
-  arrowElement.classList.add('up');
-  buttonsParent.appendChild(arrowElement);
+  const arrowUpElement = document.createElement('div');
+  if (!expandButton && !collapseButton) return;
+  arrowUpElement.classList.add('arrow', 'down');
+  expandButton.appendChild(arrowUpElement);
+  const arrowDownElement = document.createElement('div');
+  arrowDownElement.classList.add('arrow', 'up');
+  collapseButton.appendChild(arrowDownElement);
 
   expandButton.addEventListener('click', () => {
     const height = collapseBlock.querySelector('div:nth-child(2) > div').clientHeight
@@ -16,16 +18,12 @@ function addCollapseListener(collapseBlock) {
     expandableText.style.height = `${height}px`;
     collapsibleSection.classList.remove('collapsed-text');
     collapsibleSection.classList.add('expanded-text');
-    arrowElement.classList.add('down');
-    arrowElement.classList.remove('up');
   });
 
   collapseButton.addEventListener('click', () => {
     expandableText.style.height = '0';
     collapsibleSection.classList.add('collapsed-text');
     collapsibleSection.classList.remove('expanded-text');
-    arrowElement.classList.add('up');
-    arrowElement.classList.remove('down');
   });
 }
 
