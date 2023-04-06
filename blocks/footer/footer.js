b3164becac4984b6826193c5f93ce05efd9e6338
Timothee Maret <timothee.maret@gmail.com>
2023-04-06 09:56:13 +0200

    Don't append the footer in case it can't be fetched
    Co-authored-by: Satya Deep Maheshwari <m.satyadeep@gmail.com>

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -7,9 +7,11 @@ export default async function decorate(block) {
   block.textContent = '';
   const footerUrl = 'https://www.zeiss.com/semiconductor-manufacturing-technology/home.html';
   const resp = await fetch(footerUrl);
+if (resp.ok) {
   const html = await resp.text();
   const div = document.createElement('div');
   div.innerHTML = html;
   const footer = div.querySelector('footer');
   block.append(footer);
+}
 }
