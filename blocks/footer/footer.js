92cf9e27c9f5d3e79aa6dc407dfb5bce48ac28ba
tmaret <tmaret@adobe.com>
2023-04-06 12:49:37 +0200

    Add locale support

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,11 +1,19 @@
+import { getMetadata } from '../../scripts/lib-franklin.js';
+
 /**
  * loads and decorates the footer
- * @param {Element} block The header block element
+ * @param {Element} block The footer block element
  */
 
 export default async function decorate(block) {
+  const locale = getMetadata('locale');
   block.textContent = '';
-  const footerUrl = 'https://www.zeiss.com/semiconductor-manufacturing-technology/home.html';
+  const hostnames = {
+    en: 'www.zeiss.com',
+    de: 'www.zeiss.de',
+  };
+  const hostname = hostnames[locale] || hostnames.en;
+  const footerUrl = `https://${hostname}/semiconductor-manufacturing-technology/home.html`;
   const resp = await fetch(footerUrl);
   if (resp.ok) {
     const html = await resp.text();
