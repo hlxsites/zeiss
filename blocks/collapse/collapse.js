c80e92889f85d6931be8704e93ea949bdce442b1
Alina Rublea <rublea@adobe.com>
2023-04-12 13:40:56 +0300

    generic link

--- blocks/collapse/collapse.js
+++ blocks/collapse/collapse.js
@@ -11,19 +11,21 @@ function addCollapseListener(collapseBlock) {
   arrowDownElement.classList.add('arrow', 'up');
   collapseButton.querySelector('div').appendChild(arrowDownElement);
 
-  expandButton.addEventListener('click', () => {
+  expandButton.addEventListener('click', (e) => {
     const height = collapseBlock.querySelector('div:nth-child(2) > div').clientHeight
         + collapseButton.getBoundingClientRect().height
         + parseFloat(getComputedStyle(expandableText).paddingBottom);
     expandableText.style.height = `${height}px`;
     collapsibleSection.classList.remove('collapsed-text');
     collapsibleSection.classList.add('expanded-text');
+    e.preventDefault();
   });
 
-  collapseButton.addEventListener('click', () => {
+  collapseButton.addEventListener('click', (e) => {
     expandableText.style.height = '0';
     collapsibleSection.classList.add('collapsed-text');
     collapsibleSection.classList.remove('expanded-text');
+    e.preventDefault();
   });
 }
 
